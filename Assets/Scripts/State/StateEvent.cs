5a19734ee66c89eea6a4d87c4cfc543b7a6cdebf
Kit Tumbagahan <kittumbagahan@gmail.com>
2022-04-17 19:45:40 +0800

    Populate assets

---
+++ Assets/Scripts/State/StateEvent.cs
@@ -0,0 +1,13 @@
+namespace State
+{
+    public enum StateEvent
+    {
+        Start, Update, Exit
+    }
+
+    public enum States
+    {
+        // Add additional states as needed
+        Idle, Run, Walk
+    }
+}
